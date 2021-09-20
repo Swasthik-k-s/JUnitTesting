54088743090ce4500c0c3a331260db8da7e0ceed
Swasthik <swasthik.ks22@gmail.com>
2021-09-21 00:02:11 +0530

    Email ID is tested for all the sample emails

--- src/main/java/com/demo/UserRegistration.java
+++ src/main/java/com/demo/UserRegistration.java
@@ -5,8 +5,10 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	String firstNamePattern = "[A-Z][a-zA-Z]{2,}";	
 	String lastNamePattern = "[A-Z][a-zA-Z]{2,}";
-	String emailPattern = "[a-zA-Z][0-9a-zA-Z]*([.\\-_+][0-9a-zA-Z]+)?\\@[0-9a-zA-Z]+\\.[a-zA-Z]+([.][a-z]{2})?";
 	String phoneNumberPattern = "[1-9][0-9]\\s[1-9][0-9]{9}";
+	String username = "^[A-Za-z][A-Za-z0-9+-]{2}[A-Za-z0-9+-]*([.][A-Za-z0-9]{2}[A-Za-z0-9]*)?";
+	String domain = "\\@[a-zA-Z0-9]+\\.[a-z]{2}[a-z]*([.][a-z]{2}[a-z]*)?";
+	String emailPattern = username + domain;
 	String passwordPattern = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,20}";
 
 	public boolean validateFirstName(String firstname) {
